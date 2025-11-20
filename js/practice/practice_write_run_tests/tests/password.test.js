43452f36305cd7839aa35dd0cd276d8ab26dc7fc
Fatima Alkashaf <fa223n@student.lnu.se>
2025-11-20 16:19:05 +0100

    Added third test: verified trim removes leading and trailing spaces.

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -11,13 +11,14 @@
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-import { Password } from '../src/Correct'
+// import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
     const passwordWith11Characters = 'abcdefghij1'
     const passwordWith12Characters = 'abcdefghijk1'
     const expectedHashFor12Characters = 8061291001640368000
+    const passwordWithSpaces = '  abcdefghijk1  '
     
     test('constructor should throw Too short password Error for a 11 characters password', () => {
        expect(() => new Password(passwordWith11Characters)).toThrow('Too short password')
@@ -30,5 +31,18 @@ describe('Password class, test suite', () => {
 
         // Assert
         expect(actualHash).toBe(expectedHashFor12Characters)
+    })
+
+    test('getPasswordHash should return same hash for password with leading and trailing spaces', () => {
+        // Arrange
+        const passwordWithoutSpaces = new Password(passwordWith12Characters)
+        const expectedHash = passwordWithoutSpaces.getPasswordHash()
+
+        // Act
+        const passwordWithSpacesObject = new Password(passwordWithSpaces)
+        actualHash = passwordWithSpacesObject.getPasswordHash()
+
+        // Assert
+        expect(actualHash).toBe(expectedHash)
     })
 })
