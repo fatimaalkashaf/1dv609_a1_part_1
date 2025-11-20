18ad341cbf6d562560021885e8affb5a0517b8f2
Fatima Alkashaf <fa223n@student.lnu.se>
2025-11-20 14:16:39 +0100

    Added first test: constructor throws error for a 11 character password.

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,7 +1,7 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
@@ -11,15 +11,14 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
-    const emptyPassword = '';
+    const passwordWith11Characters = 'abcdefghij1'
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+    test('constructor should throw Too short password Error for a 11 characters password', () => {
 
-    //Add your tests here
-});
+       expect(() => new Password(passwordWith11Characters)).toThrow('Too short password')
+    })
+})
