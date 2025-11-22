63ef89d257a2976edaa74770fd0f7eaca45d5012
Fatima Alkashaf <fa223n@student.lnu.se>
2025-11-22 13:13:05 +0100

    Added first SSNHelper tests: isCorrectLength for valid and invalid length.

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,12 +1,29 @@
-import { SSNHelper } from '../src/correct/SSNHelper'; 
+// import { SSNHelper } from '../src/correct/SSNHelper';
+import {SSNHelper} from '../src/bugs/BuggySSNHelperWrongLength'
 
+describe('SSNHelper Tests', () => {
+    const validLengthSSN = '031006-1234'
+    const tooLongSSN = '031006-12345'
 
+    test('isCorrectLength should return true for a 11 character string', () => {
+        // Arrange
+        const helper = new SSNHelper()
+        
+        //Act
+        const result = helper.isCorrectLength(validLengthSSN)
 
-describe('SSNHelpe Tests', () => {
-
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
+        // Assert
+        expect(result).toBe(true);
     });
 
-    //Add your tests here
+    test('isCorrectLength should return false for a 12 character string', () => {
+        // Arrange
+        const helper = new SSNHelper()
+        
+        //Act
+        const result = helper.isCorrectLength(tooLongSSN)
+
+        // Assert
+        expect(result).toBe(false);
+    });
 });
